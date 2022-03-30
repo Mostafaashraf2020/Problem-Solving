5dd09ba985df60bb8f72d63a92cdfb17af1914bf
Mostafa Ashraf <mostafaats2020@gmail.com>
2022-03-31 01:38:53 +0200

    Sheet1-problemA-Say-Hello

---
+++ Contest1/Say Hello/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String word = obj.nextLine();
+        System.out.println("Hello," + " " + word);
+
+    }
+}
